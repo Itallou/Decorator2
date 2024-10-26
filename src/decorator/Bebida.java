445290b26e2b6dbcaf84c1d825dc0e2e3c6a9dc0
Itallou <itallosilvasd@gmail.com>
2024-10-26 20:14:41 -0300

    Decorator

---
+++ src/decorator/Bebida.java
@@ -0,0 +1,7 @@
+package decorator;
+
+public interface Bebida {
+    String obterDescricao();
+    double obterCusto();
+    
+}
---
+++ src/decorator/ChocolateDecorator.java
@@ -0,0 +1,18 @@
+package decorator;
+
+public class ChocolateDecorator extends BebidaDecorator {
+
+    public ChocolateDecorator(Bebida bebida) {
+        super(bebida);
+    }
+
+    @Override
+    public String obterDescricao() {
+        return super.obterDescricao() + ", Chocolate";
+    }
+
+    @Override
+    public double obterCusto() {
+        return super.obterCusto() + 3.0;
+    }
+}
